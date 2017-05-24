f3db3c1679f698e6827c2ffaa84893f9ca66ced3
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-05-23 21:18:04 -0700

    Click handler to show list of all trip is done

---
+++ trek.js
@@ -0,0 +1,47 @@
+
+var listUrl =  "https://trektravel.herokuapp.com/trips";
+
+
+var successCallback = function(response) {
+  console.log("Success!");
+  console.log(response);
+  var trips = _.template($('#trips').html());
+  for (var i = 0; i < response.length; i++) {
+    var generatedHtml = trips({
+      data: response[i]
+    });
+
+    $("#alltrips table").append(generatedHtml);
+  }
+
+  $('.trip-link').click(function(event){
+    console.log("clicked on trip-link");
+    // data is jQuery function, shows in the console object{tripId: 2}
+    console.log("trip_id = " , $(this).data().tripId);
+    var id = $(this).data().tripId;
+    $.get(listUrl + "/" + id , showTripCallback).fail(failureCallback);
+  });
+};
+
+
+var showTripCallback= function(response){
+  $("#alltrips").hide();
+
+};
+
+var failureCallback = function() {
+  console.log("Didn't work :(");
+  $("#errors").html("<h1>AJAX request failed!</h1>");
+};
+
+var clickHandler = function(event) {
+  //$.get(url, successCallback);
+  $.get(listUrl, successCallback).fail(failureCallback);
+};
+
+$(document).ready(function() {
+  // Associate the click handler
+  $('#load').click(clickHandler);
+
+
+});
