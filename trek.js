e9e19a737618bcaaefe361a1c528932a5b61feea
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-05-25 15:40:34 -0700

    Cleaned and divided the code into small methods to enhance code readability

--- trek.js
+++ trek.js
@@ -4,7 +4,6 @@ var listUrl =  "https://trektravel.herokuapp.com/trips";
 var successCallback = function(response) {
   console.log("Success!");
   console.log(response);
-  //$("#show-trip").empty();
 
   var trips = _.template($('#trips').html());
   for (var i = 0; i < response.length; i++) {
@@ -14,48 +13,15 @@ var successCallback = function(response) {
 
     $("#alltrips table").append(generatedHtml);
   }
-
-  // showTripClickHandler
-  $('.trip-link').click(function(event){
-    console.log("clicked on trip-link");
-    // data is jQuery function, shows in the console object{tripId: 2}
-
-    console.log("trip_id = " , $(this).data().tripId);
-    var id = $(this).data().tripId;
-    $.get(listUrl + "/" + id , showTripCallback).fail(failureCallback);
-  });
+  // showTripClickHand
+  $('.trip-link').click(showTripClickHandler);
 };
 
-var showTripCallback= function(response){
-  // $("#alltrips").hide();
-  //  $("#alltrips table").empty();
-   $("#alltrips table").hide();
+var clickHandler = function(event) {
+  $("#show-trip").empty();
+  $("#alltrips table").show();
 
-// Show the trip information
-  var trip = _.template($('#one-trip').html());
-    var generatedHtml = trip({
-      data: response
-    });
-
-    $("#show-trip").append(generatedHtml);
-
-    // Submit post to reserve a spot
-    $('form').submit(function(e){
-      e.preventDefault();
-      console.log("we are here");
-
-      var url = $(this).attr("action");
-      var formData = $(this).serialize();
-
-      $.post(url, formData, function(response){
-        $('#message').html('<p> Thanks for the reservation </p>');
-
-        console.log(response);
-      })
-      .fail(function(){
-        $('#message').html("<p>Sorry, You couldn't reserve for this trip</p>");
-    });
-  });
+  $.get(listUrl, successCallback).fail(failureCallback);
 };
 
 var failureCallback = function() {
@@ -63,17 +29,48 @@ var failureCallback = function() {
   $("#errors").html("<h1>AJAX request failed!</h1>");
 };
 
-var clickHandler = function(event) {
-  $("#show-trip").empty();
-  $("#alltrips table").show();
+// Show one trip information
+var showTripClickHandler = function(event){
+  console.log("clicked on trip-link");
 
+// data is jQuery function, shows in the console object{tripId: 2}
+  console.log("trip_id = " , $(this).data().tripId);
+  var id = $(this).data().tripId;
+  $.get(listUrl + "/" + id , showTripCallback).fail(failureCallback);
+};
 
-  $.get(listUrl, successCallback).fail(failureCallback);
+var showTripCallback= function(response){
+   $("#alltrips table").hide();
+
+  var trip = _.template($('#one-trip').html());
+    var generatedHtml = trip({
+      data: response
+    });
+
+    $("#show-trip").append(generatedHtml);
+
+  // FormSubmitEventHabdler
+    $('form').submit(formSubmitHandler);
+};
+
+var formSubmitHandler = function(e){
+  e.preventDefault();
+  console.log("we are here");
+
+  var url = $(this).attr("action");
+  var formData = $(this).serialize();
+
+  $.post(url, formData, function(response){
+    $('#message').html('<p> Thanks for the reservation </p>');
+
+    console.log(response);
+  }).fail(function(){
+    $('#message').html("<p>Sorry, You couldn't reserve for this trip</p>");
+  });
 };
 
 $(document).ready(function() {
   $("#alltrips table").hide();
 
   $('#load').click(clickHandler);
-  // $('#load').on("click",'table',clickHandler);
 });
