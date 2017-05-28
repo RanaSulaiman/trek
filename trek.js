1de016d41ef873404b88f346721415722ae4e6c4
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-05-28 12:48:06 -0700

    Fixed header with all buttons to be responsive

--- trek.js
+++ trek.js
@@ -101,5 +101,5 @@ $(document).ready(function() {
   $('#load').click(clickHandler);
 
   // Filter by Continenet
-  $('header').on("change","#trips-continent", filterClickHandler);
+  $('header').on("submit","#trips-continent", filterClickHandler);
 });
